663f433170b56fcf3c0cf916854648b2509306d7
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-03 03:10:29 +0200

    init app

---
+++ StickerSmash/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+const PlaceHolderImage = require("./assets/images/background-image.png")
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceHolderImage} style={styles.image}/>
+      </View>
+      <Text style={{color:"#fff"}}>This is my first app</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer:{
+    flex:1,
+    paddingTop:58
+  },
+  image:{
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  }
+});
