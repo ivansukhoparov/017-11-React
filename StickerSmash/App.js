540ba10926cc1e3debd73693ee165c341645f642
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-05 01:24:18 +0200

    2,3,4 tutorial's chapters done

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,38 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from 'react';
+import IconButton from "./components/IconButton";
+import CircleButton from "./components/CirculButton";
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiSticker from './components/EmojiSticker';
+import EmojiList from "./components/EmojiList";
 
 const PlaceHolderImage = require("./assets/images/background-image.png")
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceHolderImage} />
-      </View>
-        <View style={styles.footerContainer}>
-            <Button theme = "primary" label="Choose a photo"/>
-            <Button label="Use this photo"/>
+    const [selectedImage, setSelectedImage] = useState(null)
+    const [showAppOptions, setShowAppOptions] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [pickedEmoji, setPickedEmoji] = useState(null);
+    const pickImageAsync = async () => {
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+        });
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri)
+            setShowAppOptions(true);
+            console.log(result);
+        } else {
+            alert('You did not select any image.');
+        }
+    };
+    const onReset = () => {
+        setShowAppOptions(false);
+    };
+
+    const onSaveImageAsync = async () => {
+        // we will implement this later
+    };
+
+    const onAddSticker = () => {
+        setIsModalVisible(true);
+    };
+
+    const onModalClose = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={PlaceHolderImage} selectedImage={selectedImage}/>
+                {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+            </View>
+
+            {showAppOptions ? (
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton icon="refresh" label="Reset" onPress={onReset} />
+                        <CircleButton onPress={onAddSticker} />
+                        <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+                    </View>
+                </View>
+            ) : (
+                <View style={styles.footerContainer}>
+                    <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+                    <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+                </View>
+            )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+            </EmojiPicker>
+            <Text style={{color: "#fff"}}>This is my first app</Text>
+            <StatusBar style="auto"/>
         </View>
-      <Text style={{color:"#fff"}}>This is my first app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imageContainer:{
-    flex:1,
-    paddingTop:58
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58
+    },
     footerContainer: {
         flex: 1 / 3,
         alignItems: 'center',
+    },
+    optionsContainer: {
+        position: 'absolute',
+        bottom: 80,
+    },
+    optionsRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
     },
 });
--- StickerSmash/components/Button.js
+++ StickerSmash/components/Button.js
@@ -1,19 +1,24 @@
-import {Pressable, StyleSheet, Text, View} from "react-native";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+    Pressable, // button
+    StyleSheet, // component to create styles
+    Text, // block of text
+    View // container
+} from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome"; // library with vector icons from expo
 
-export default function Button({label, theme}) {
+export default function Button({label, theme, onPress}) {
     if (theme === "primary") {
         return (
             <View style={styles.buttonContainer}>
                 <Pressable style={[styles.button, {backgroundColor: "#fff"}]}
-                           onPress={() => alert('You pressed a button.')}>
+                           onPress={onPress}>
                     <FontAwesome
                         name="picture-o"
                         size={18}
                         color="#25292e"
                         style={styles.buttonIcon}
                     />
-                    <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+                    <Text style={[styles.buttonLabel, {color: "#25292e"}]}>{label}</Text>
                 </Pressable>
             </View>
         )
@@ -21,7 +26,7 @@ export default function Button({label, theme}) {
 
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Pressable style={styles.button} onPress={onPress} >
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
@@ -31,7 +36,7 @@ export default function Button({label, theme}) {
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        width: 320,
+        width: 100,
         height: 68,
         marginHorizontal: 20,
         alignItems: 'center',
